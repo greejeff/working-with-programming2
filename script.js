a30f0448659cfb4090d2f90e10e51b6a5a534ea5
greejeff <71346520+greejeff@users.noreply.github.com>
2020-09-21 22:30:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var boxEle = document.body.querySelector(".box");
+
+var userInput = prompt("Select a number.")
+var userInput = prompt("Select a second number.")
+
+var numberOne = 10;
+var numberTwo = 20;
+
+boxEle.innerHTML=userInput;
+
+boxEle.innerHTML=numberOne+numberTwo;
